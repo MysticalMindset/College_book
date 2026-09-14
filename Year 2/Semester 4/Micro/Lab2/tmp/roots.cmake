45e92015a37de035c9358b95c6267023c1fccab9
Michael McCorkell <itstoxication@gmail.com>
2026-09-14 12:17:50 -0400

    better upload, zip
    Zip file uploaded for all Labs and Project

--- Year 2/Semester 4/Micro/Lab2/tmp/roots.cmake
+++
@@ -1,7 +0,0 @@
-# roots.cmake
-set(CMSIS_PACK_ROOT "C:/Users/Murl0/AppData/Local/Arm/Packs" CACHE PATH "CMSIS pack root")
-cmake_path(ABSOLUTE_PATH CMSIS_PACK_ROOT NORMALIZE OUTPUT_VARIABLE CMSIS_PACK_ROOT)
-set(CMSIS_COMPILER_ROOT "C:/Users/Murl0/.vscode/extensions/arm.cmsis-csolution-1.66.0-win32-x64/tools/cmsis-toolbox/etc" CACHE PATH "CMSIS compiler root")
-cmake_path(ABSOLUTE_PATH CMSIS_COMPILER_ROOT NORMALIZE OUTPUT_VARIABLE CMSIS_COMPILER_ROOT)
-set(SOLUTION_ROOT "C:/Users/Murl0/Documents/Workzone/College_book/Year 2/Semester 4/Micro/Lab2" CACHE PATH "CMSIS solution root")
-cmake_path(ABSOLUTE_PATH SOLUTION_ROOT NORMALIZE OUTPUT_VARIABLE SOLUTION_ROOT)
